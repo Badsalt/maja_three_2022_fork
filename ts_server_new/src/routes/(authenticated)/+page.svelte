2e27cc8ceb43987381b87fe0e0ec322914e3e55a
Badsalt <meldeg@edu.umea.se>
2022-10-19 10:43:19 +0200

    Revert "working copy 2"
    This reverts commit a90843b97df8ef0fb88f87345c8077ba2ec4acab.

--- ts_server_new/src/routes/(authenticated)/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts" context="module">
-  import type { LayoutServerData } from "./$types";
-  export let data: LayoutServerData;
-</script>
-
-<h1>LOGGED IN</h1>
--- ts_server_new/src/routes/(shared)/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import type { LayoutServerLoad } from "./$types";
-
-export const load: LayoutServerLoad = async ({ locals, cookies }) => {
-  if (locals.user) {
-    return {
-      user: locals.user.data,
-    };
-  }
-};
